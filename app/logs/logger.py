036486430489ee4a8dea9fc1584f8110657507c1
deep-div <div20023041@gmail.com>
2025-12-08 19:35:28 +0530

    copying main code

--- app/logs/logger.py
+++ app/logs/logger.py
@@ -1,31 +1,25 @@
 import logging
-from logging.handlers import RotatingFileHandler
+import sys
 
 def get_logger(name: str = "app_logger") -> logging.Logger:
-    level= logging.DEBUG
-    
+    level = logging.DEBUG
+
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
     if not logger.handlers:  # Avoid duplicate handlers
-        # File Handler (Rotating)
-        file_handler = RotatingFileHandler(
-            "app/logs/app.log",
-            maxBytes=5 * 1024 * 1024,  # 5MB
-            backupCount=2
-        )
-        file_handler.setLevel(level)
+        # Stream Handler (STDOUT)
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setLevel(level)
 
         formatter = logging.Formatter(
-            "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+            "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
         )
-        file_handler.setFormatter(formatter)
+        stream_handler.setFormatter(formatter)
 
-        logger.addHandler(file_handler)
-
-        # Disable propagation to avoid root logger printing to console
-        logger.propagate = False
+        logger.addHandler(stream_handler)
 
     return logger
+
 
 logger = get_logger()
